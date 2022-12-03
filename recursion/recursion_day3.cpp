f5702ee4682ed16ae7b11c7a0ebd5eaa0eb30b37
Abhishek Sharma <90740003+AbhishekShaarmaa@users.noreply.github.com>
2022-12-03 03:30:38 +0000

    day3

---
+++ recursion/recursion_day3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// topics --- recursion and binary search
+
+
+bool name(int arr[], int size)
+{
+    if (size==0 || size==1)
+    {
+        return true;
+    }
+   
+    if ( arr[0]>arr[1])
+    {
+        return 
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+
+    string arr[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+return 0;
+}
---
+++ recursion/recursionsum.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+int sum(int *arr , int size) 
+{
+    if(size==0){
+        return 0;
+    }
+    if(size==1){
+        return arr[0];
+    }
+
+    int get
+}
+int main()
+
+{
+int size;
+cout<<"size"<<endl;
+cin >> size;
+
+int arr[size];
+for(int i=0 ; i<=size ; i++ )
+{
+    arr[i]=arr[size];
+    cin >> arr[i];
+   
+}
+
+return 0;
+}
